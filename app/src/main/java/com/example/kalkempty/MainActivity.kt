329de46eb5bb0969f821e6fdc8fbe386cb719328
Marcin Kołacz <marcinkolacz1976@gmail.com>
2021-10-06 07:41:28 +0200

    Dodany przycisk nr 1

---
+++ app/src/main/java/com/example/kalkempty/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.kalkempty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        var wynik = 0;
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //findViewById<Button>(R.id.button).text.toString()
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<Button>(R.id.button).text = "0";
+        }
+
+        findViewById<Button>(R.id.nr1).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text = "1"
+        }
+    }
+}
